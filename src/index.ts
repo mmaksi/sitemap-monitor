33a0127123aa51c8d8f5e009932cae7f9fcc11b7
Mark Maksi <mmaksi.dev@gmail.com>
2024-11-12 09:32:50 +0100

    updated port

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { app } from './app';
-import http from 'http';
 
-const hostname = '0.0.0.0';
-const port = 80;
+const port = 3000;
 
-http.createServer(app).listen(port, hostname, () => {
+app.listen(port, () => {
   console.log('Server running on port 3000');
 });
