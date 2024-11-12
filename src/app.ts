2203d02a3c42e07c0c35678504b69ad79906267c
Mark Maksi <mmaksi.dev@gmail.com>
2024-11-12 13:39:21 +0100

    updated cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,19 @@
-import express from "express";
-import "express-async-errors";
-import dotenv from "dotenv";
-import api from "./routes/api";
-import { errorHandler } from "@eventexchange/common";
-
+import express from 'express';
+import 'express-async-errors';
+import dotenv from 'dotenv';
+import api from './routes/api';
+import { errorHandler } from '@eventexchange/common';
+import cors from 'cors';
 dotenv.config();
 export const app = express();
 
 // Important middlewares
 app.use(express.json());
 
+app.use(cors());
+
 // Router mounting
-app.use("/api", api);
+app.use('/api', api);
 
 // Error handling
 app.use(errorHandler);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from './app';
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => {
-  console.log('Server running on port 3000');
+  console.log('Server running on port 5000');
 });
